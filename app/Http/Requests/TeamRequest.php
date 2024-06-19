55d5fd92e3d4d752ffb2ed048c2862c202d40f7a
unknown <mohammadelia30@gmail.com>
2024-06-20 01:26:51 +0330

    add request

---
+++ app/Http/Requests/TeamRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TeamRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|min:2|max:50',
+            'activity' => 'required|min:2|max:150',
+            'leader' => 'required|min:2|max:50',
+            'member' => 'required|integer',
+        ];
+    }
+}
