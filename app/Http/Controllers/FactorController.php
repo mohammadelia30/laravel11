6e06feb4b46c0f8626c94e320d3d305ac572587c
unknown <mohammadelia30@gmail.com>
2024-06-20 01:26:18 +0330

    add media to controller

--- app/Http/Controllers/FactorController.php
+++ app/Http/Controllers/FactorController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\FactorRequest;
+use App\Models\Factor;
 use Illuminate\Http\Request;
 
 class FactorController extends Controller
 {
-    //
+    public function index($id = null)
+    {
+        if ($id) {
+            $factor = Factor::with('order','warranty')->find($id);
+        } else {
+            $factor = Factor::with('order')->paginate(10);
+        }
+        return response()->json($factor);
+    }
+    public function store(FactorRequest $request)
+    {
+        $factor = Factor::create($request->toArray());
+        return response()->json($factor);
+    }
+    public function edit(FactorRequest $request){
+        $factor = Factor::update($request->toArray());
+        return response()->json($factor);
+    }
+    public function delete($id)
+    {
+        $factor = Factor::destroy($id);
+        return response()->json($factor);
+    }
 }
